b7758eb05ce24773cb1050824f19abde91995d2c
takuya20221023 <takuya20221023@gmail.com>
2025-03-04 16:39:43 +0900

    ログインログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
  validates :nickname, presence: true
-  validates :first_name, presence: true
+ validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,7 +6,12 @@ RSpec.describe User, type: :model do
       email: "test@example.com",
       password: "password",
       password_confirmation: "password",  # 追加: password_confirmationを指定
-      nickname: "testuser"
+      nickname: "testuser",
+      last_name: "山田" ,
+      first_name: "太郎" ,
+      last_name_kana: "ヤマダ" ,
+      first_name_kana: "タロウ" ,
+      birth_date: "1990-01-01" ,
     )
     expect(user).to be_valid
   end
@@ -29,4 +34,32 @@ RSpec.describe User, type: :model do
     )
     expect(user).not_to be_valid
   end
+
+  it "is invalid without a nickname" do
+    user = User.new(nickname: "testuser")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without a last_name " do
+    user = User.new(last_name: "山田")
+    expect(user).not_to be_valid
+  end
+  it "is invalid without a first_name " do
+    user = User.new(first_name: "太郎")
+    expect(user).not_to be_valid
+  end
+  it "is invalid without a last_name_kana" do
+    user = User.new(last_name_kana: "ヤマダ")
+    expect(user).not_to be_valid
+  end
+  it "is invalid without a first_name_kana" do
+    user = User.new(first_name_kana: "タロウ")
+    expect(user).not_to be_valid
+  end
+  it "is invalid without a birth_date" do
+    user = User.new(birth_date: "1990-01-01")
+    expect(user).not_to be_valid
+  end
+
+  
 end
